a843878535fc363af9e2841a29eb75eea3edf692
Keifer <lewiskeifer@yahoo.com>
2017-11-11 16:44:54 -0500

    Added Pencil, Paper, Utility - passes first test

--- main_test.cpp
+++ main_test.cpp
@@ -1,13 +1,23 @@
 #include "Pencil.h"
+#include "Paper.h"
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
 int main()
 {
-	Pencil pencil;
-	pencil.write("Hello world!");
+	const int pointDurability = 1000;
+	const int eraserDurability = 1000;
 
+	Paper* paper = new Paper(0, 1000);
+	Pencil pencil(0, 10, pointDurability, eraserDurability);
+
+	string test = "Hello world!";
+
+	pencil.write(test, paper);
+
+	assert(pencil.getPointDurability() == (pointDurability - test.length()));
 
 	return 0;
 }
