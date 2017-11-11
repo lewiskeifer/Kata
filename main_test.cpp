225fb42bc703e55e154b1d3ed771c517819ef8ed
Keifer <lewiskeifer@yahoo.com>
2017-11-11 17:04:20 -0500

    Passes second test

--- main_test.cpp
+++ main_test.cpp
@@ -6,17 +6,22 @@
 using namespace std;
 
 void testPencilAndPaperWrite(Pencil& pencil, Paper* paper);
+void testPencilDurabilityAndPaperLength(Pencil& pencil, Paper* paper);
+void testPencilSharpen(Pencil& pencil);
 
 const int pointDurability = 1000;
 const int eraserDurability = 1000;
+const int paperLength = 1000;
 
 
 int main()
 {
 	Pencil pencil(0, 10, pointDurability, eraserDurability);
-	Paper* paper = new Paper(0, 1000);
+	Paper* paper = new Paper(0, paperLength);
 
 	testPencilAndPaperWrite(pencil, paper);
+	testPencilDurabilityAndPaperLength(pencil, paper);
+	testPencilSharpen(pencil);
 
 	return 0;
 }
@@ -29,5 +34,26 @@ void testPencilAndPaperWrite(Pencil& pencil, Paper* paper)
 	assert(pencil.getPointDurability() == (pointDurability - test.length()));
 	assert(paper->getCurrentLength() == test.length());
 
-	cout << *paper;
+	cout << *paper << endl;
+}
+
+void testPencilDurabilityAndPaperLength(Pencil& pencil, Paper* paper)
+{
+	int currentDurability = pencil.getPointDurability();
+
+	for (int i = 0; i < currentDurability; ++i)
+	{
+		string s = to_string(i);
+		pencil.write(s, paper);
+	}
+
+	assert(pencil.getPointDurability() == 0);
+	assert(paper->getCurrentLength() == paperLength);
+
+	cout << *paper << endl;
+}
+
+void testPencilSharpen(Pencil& pencil)
+{
+
 }
